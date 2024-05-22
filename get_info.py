301a1ec162d3ea6230aac2df683b07de107d38de
ShDmytros <160020732+ShDmytros@users.noreply.github.com>
2024-05-22 10:24:40 +0300

    Add files via upload

--- get_info.py
+++ get_info.py
@@ -2,59 +2,78 @@ from parsel import Selector
 import requests
 from requests import Response
 import re
+import random
+from key_user_agent import user_agent
 
 
 # pip install parsel, requests
 
 
 class parsing:
-    def __init__(self, text):
-        self.base_information = None
-        self.final_list = None
-
+    def __init__(self, text, lang):
         self.text = text
-        self.url: str = "https://uk.wikipedia.org/wiki"
+        self.lang = lang
+        if self.lang == "ua":
+            self.url: str = "https://uk.wikipedia.org/wiki"
+            # print("Ukrainian")
+        elif self.lang == "en":
+            self.url: str = "https://en.wikipedia.org/wiki"
+            # print("English")
         self.new_url: str = self.url + "/" + self.text
 
         self.headers = self.get_headers()
         self.response: Response = self.make_request()
-        self.data_list = self.parse()
+
         self.html: Response = requests.get(self.new_url)
+        self.selector = Selector(text=self.response.text)
+        self.data_list = self.parse()
 
     def make_request(self) -> Response:
         return requests.get(url=self.new_url, headers=self.headers)
 
     def get_headers(self) -> dict:
         return {
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                          'Chrome/122.0.0.0 Safari/537.36',
+            'user-agent': str(random.sample(user_agent, 32))
         }
 
-    def parse(self) -> str:
-        self.base_information = {}
-        selector = Selector(text=self.response.text)
+    # def right_menu(self):
+    #     base_information = {}
+    #
+    #     base_information_main = self.selector.css('table.infobox tbody tr th::text').getall()
+    #
+    #     # print(base_information_main)
+    #     # for clear_index in range(len(base_information_main)):
+    #     #     print(clear_index)
+    #     #     if '\n' in base_information_main[clear_index]:
+    #     #         base_information_main[clear_index] = re.sub('\n', '', base_information_main[clear_index])
+    #     #         if base_information_main[clear_index] == '':
+    #     #             base_information_main.pop(clear_index)
+    #     #             print(base_information_main)
+    #
+    #     # print(base_information_main)
+    #
+    #     base_information_info = self.selector.css('table.infobox tbody tr td::text').getall()
+    #     # base_information_info = re.sub(r'\d+')
+    #
+    #     for base_main, base_info in zip(base_information_main, base_information_info):
+    #         base_information[base_main] = base_info
+    #
+    #     return base_information
 
-        base_information_main = selector.css('table.infobox tbody tr th::text').getall()
-        base_information_info = selector.css('table.infobox tbody tr td::text').getall()
+    def information(self):
+        if self.lang == "ua":
+            information: str = str(self.selector.xpath('//*[@id="mw-content-text"]/div[1]/p[1]').get())
+        elif self.lang == "en":
+            information: str = str(self.selector.xpath('//*[@id="mw-content-text"]/div[1]/p[2]').get())
 
-        for base_main, base_info in zip(base_information_main, base_information_info):
-            self.base_information[base_main] = base_info
-
-        print(self.base_information)
-
-        information: str = str(selector.xpath('//*[@id="mw-content-text"]/div[1]/p[1]').get())
         information: Selector = Selector(text=information)
         clean_information = information.xpath('string()').extract_first()
         clean_information = re.sub(r'\[\d+\]', '', clean_information)
 
-        self.final_list = clean_information + "\n" + self.new_url
-        return self.final_list
+        return clean_information + "\n" + self.new_url
 
-
-def main() -> None:
-    data_parse = parsing(text='Херсон')
-    parsing_info = data_parse.data_list
-
-
-if __name__ == '__main__':
-    main()
+    def parse(self) -> str:
+        # print(self.selector.type)
+        # final_list = "right_menu" + str(self.right_menu()) + "information" + str(self.information())
+        final_list = str(self.information())
+        return final_list
--- main.py
+++ main.py
@@ -1,27 +1,92 @@
 from get_info import parsing
-from my_key import key
+from data.my_key import key
 import telebot
+from telebot import types
 
 # pip install pyTelegramBotAPI
 
 
 bot = telebot.TeleBot(key)
+language = None
 
 
 @bot.message_handler(commands=['start'])
-def main(message):
-    bot.send_message(message.chat.id, "Це альфа версія Вікіпедії бота!")
-    bot.send_message(message.chat.id, "1.Доступна лише українська мова🇺🇦, з будь якими іншими мовами бот працювати не буде!\n2.Пишіть обов'язково коректно.")
+def welcome(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("/start")
+    item2 = types.KeyboardButton("/ukrainian")
+    item3 = types.KeyboardButton("/english")
+    markup.add(item1, item2, item3)
 
-@bot.message_handler()
-def main(message):
-    data = parsing(text=message.text)
-    data_info = str(data.data_list)
+    # ukrainian
+    bot.send_message(message.chat.id, "Українська мова🇺🇦")
+    bot.send_message(message.chat.id, "Це альфа версія Вікіпедії бота! Якщо будуть якісь помилки, пишіть @ShDmytros")
+    bot.send_message(message.chat.id,
+                     "1.Доступна лише українська мова🇺🇦 та англійська мова🇬🇧, з будь якими іншими мовами бот "
+                     "працювати не буде!\n\n2.Пишіть обов'язково коректно.")
+    bot.send_message(message.chat.id,
+                     'Вкажіть мову спілкування, щоб обрати мову спілкування, напишіть команду, для української '
+                     '/ukrainian, та для англійської /english')
+
+    # english
+    bot.send_message(message.chat.id, "English language🇬🇧")
+    bot.send_message(message.chat.id, "This is an alpha version of the bot's Wikipedia! If there are any errors, "
+                                      "write to @ShDmytros")
+    bot.send_message(message.chat.id,
+                     "1.Only Ukrainian🇺🇦 and English🇬🇧 are available, with any other languages the bot"
+                     "will not work!\n\n2.Be sure to write correctly.")
+    bot.send_message(message.chat.id,
+                     'Specify the language of communication to select the language of communication, write a command, '
+                     'for Ukrainian /ukrainian and for English /english', reply_markup=markup)
+
+
+@bot.message_handler(commands=['ukrainian'])
+def ua(lang):
+    bot.send_message(lang.chat.id,
+                     'Ви обрали українську мову🇺🇦, якщо Ви хочете змінити мову, напишіть команду /start та поторіть '
+                     'все зпочатку. ')
+    bot.send_message(lang.chat.id, 'Тепер напишіть, що Ви хочете знайти, до прикладу "Лос '
+                                   'Андежелес" або "Херсон" P.S: якщо те, що Ви шукаєте складаєть більше ніж з одного '
+                                   'слова,'
+                                   'до прикладу "Лос Анджелес", то пишіть це через нижнє підкреслювання, приклад '
+                                   '"Лос_Анджелес" або'
+                                   '"Лос-Анджелес"')
+
+    global language
+    language = "ua"
+
+
+@bot.message_handler(commands=['english'])
+def en(lang):
+    bot.send_message(lang.chat.id,
+                     'You have chosen English language🇬🇧, if you want to change the language, write the command '
+                     '/start and start over.')
+    bot.send_message(lang.chat.id, 'Now write what you want to find, e.g. ‘Los Angeles’ or ‘Kherson’ P.S.: '
+                                   'if what you are looking for is more than one word, ‘e.g. “Los Angeles”, '
+                                   'then write it with'
+                                   'lowercase underscores, e.g. “Los_Angeles” or “Los Angeles”')
+
+    global language
+    language = "en"
+
+
+@bot.message_handler(content_types=["text"])
+def message_handler(message):
+    if language == "ua":
+        data = parsing(text=message.text, lang="ua")
+    elif language == "en":
+        data = parsing(text=message.text, lang="en")
+    else:
+        bot.send_message(message.chat.id, "Будь ласка, спочатку оберіть мову командою /start.")
+        bot.send_message(message.chat.id, "Please first select the language with the /start command.")
+        return
+
+    data_info: str = str(data.data_list)
+
     if "None" in data_info:
-        bot.send_message(message.chat.id, "Інформацію не знайдено!")
+        bot.send_message(message.chat.id, "Інформацію не знайдено!" if language == "ua" else "Information not found!")
     else:
         bot.send_message(message.chat.id, text=data_info)
 
+
 bot.polling(non_stop=True)
-
-
